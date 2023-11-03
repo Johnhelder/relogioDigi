49173413d956fd3691e8c87b59616f7b80d27d3d
John Helder <johnhelderjp@gmail.com>
2023-11-02 21:48:08 -0300

    Criando as tags

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const div_data = document.getElementById("div_data");
+const div_relogio = document.getElementById("div_relogio");
+const btn_ativar = document.getElementById("btn_ativar");
+const btn_parar = document.getElementById("btn_parar");
+const hora_alarme = document.getElementById("hora_alarme");
+const timer = document.getElementById("timer");
