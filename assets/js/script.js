60ab0c657e86cc123062ab417769bd60480ccc98
John Helder <johnhelderjp@gmail.com>
2023-11-03 22:07:22 -0300

    Criando variaveis

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,11 @@ const btn_ativar = document.getElementById("btn_ativar");
 const btn_parar = document.getElementById("btn_parar");
 const hora_alarme = document.getElementById("hora_alarme");
 const timer = document.getElementById("timer");
+
+const som_alarme = new Audio("alarme.mp3");
+
+som_alarme.loop=-1;
+
+
+
+const data = new Date();
